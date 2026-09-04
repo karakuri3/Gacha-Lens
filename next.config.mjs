e72087932172520b07282acf8428cc11adcb1790
karakuri3 <senpingxingzuo@gmail.com>
2026-09-04 22:26:28 +0900

    chore: remove ineffective next config cache experiment

--- next.config.mjs
+++ next.config.mjs
@@ -9,15 +9,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/series/(.*)",
-        headers: [
-          {
-            key: "CDN-Cache-Control",
-            value: "public, max-age=1800, stale-while-revalidate=60",
-          },
-        ],
-      },
-      {
         source: "/(.*)",
         headers: [
           {
